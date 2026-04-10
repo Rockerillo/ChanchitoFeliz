3ff9a2a05142b798f74321fc64399ca93154ac8d
moises-pacheco <zmoises.pacheco@gmail.com>
2026-04-10 14:09:36 +0200

    cambiox xS

---
+++ Ejercicio1FranHerencia/src/controller/Calculadora.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class Calculadora {
+    private int n1;
+    private int n2;
+
+    public Calculadora(int n1, int n2){
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+
+    public void suma(){
+        int resultado = n1 + n2;
+        System.out.println(resultado);
+    }
+}
